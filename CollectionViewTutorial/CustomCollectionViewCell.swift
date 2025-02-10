9253ba2cca42ba253060a9cc7c8b47f1f4c6d65d
1140074 <alejandro.guerrah@elektra.com.mx>
2025-02-10 16:16:16 -0600

    finalizado

---
+++ CollectionViewTutorial/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewTutorial
+//
+//  Created by Alejandro Guerra Hernandez on 10/02/25.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
