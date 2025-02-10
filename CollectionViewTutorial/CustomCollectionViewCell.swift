d3e15d9973000d6067feafecd40034ba8a490c12
1140074 <alejandro.guerrah@elektra.com.mx>
2025-02-10 16:16:44 -0600

    preparando todo

--- CollectionViewTutorial/CustomCollectionViewCell.swift
+++ CollectionViewTutorial/CustomCollectionViewCell.swift
@@ -9,4 +9,32 @@ import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "CustomCollectionViewCell"
+    
+    private let myImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(systemName: "questionmark")
+        iv.tintColor = .black
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    public func configure(image: UIImage) {
+        myImageView.image = image
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = .systemRed
+        
+        self.addSubview(myImageView)
+        myImageView.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.myImageView.image = UIImage(systemName: "questionmark")
+    }
+    
 }
--- CollectionViewTutorial/ViewController.swift
+++ CollectionViewTutorial/ViewController.swift
@@ -8,12 +8,94 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var images = [UIImage]()
+    private var sideInset:CGFloat = 15
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .systemBackground
+        collection.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        //collection.contentInset = UIEdgeInsets(top: 100, left: sideInset, bottom: 100, right: sideInset)
+        collection.alwaysBounceVertical = false
+        collection.alwaysBounceHorizontal = false
+        collection.showsVerticalScrollIndicator = false
+        //collection.contentInsetAdjustmentBehavior = .never
+        return collection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupUI()
+        
+        for _ in 0...25 {
+            images.append(UIImage(named: "1")!)
+            images.append(UIImage(named: "2")!)
+            images.append(UIImage(named: "3")!)
+            images.append(UIImage(named: "4")!)
+            images.append(UIImage(named: "5")!)
+        }
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
     }
 
+    private func setupUI() {
+        view.backgroundColor = .systemBlue
+        
+        view.addSubview(collectionView)
+        collectionView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as? CustomCollectionViewCell else {
+            fatalError("Failed to dequeue CustomCollectionViewCell inHomeController")
+        }
+        
+        let image = self.images[indexPath.row]
+        cell.configure(image: image)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected: \(indexPath.row)")
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.width - 2*sideInset)/3 - 1.34
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 100, left: sideInset, bottom: 100, right: sideInset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
