762842bf95925308cd95d17425dc96904c797341
1140074 <alejandro.guerrah@elektra.com.mx>
2025-02-10 10:37:55 -0600

    Initial Commit

---
+++ CollectionViewTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTutorial
+//
+//  Created by Alejandro Guerra Hernandez on 10/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
